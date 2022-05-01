b04136c810fdbab14b6c9ada37dfc8171ad1ced1
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2022-05-01 12:53:49 +0200

    Drugi commit - bez reset

---
+++ calculator.py
@@ -0,0 +1,54 @@
+firstNumber = None
+secondNumber = 0
+operation = None
+reset = True
+result = 0
+operationCalc = ["+", "-", "*", "/", "**"]
+num = None
+
+sum = 0
+
+while True :
+    if firstNumber == None :
+        reset = True
+        firstNumber = int(input ("Podaj pierwszą liczbę : "))
+    else :
+        reset = False
+        num =  input ("Podaj działanie " + str(operationCalc) + " lub exit lub reset: ")
+        if num == "exit" :
+            break
+        if num == "reset" :
+            reset = True
+            continue
+        if num == "+" :
+            secondNumber = int (input("Podaj drugą liczbę : "))
+            result = firstNumber + secondNumber
+            firstNumber = result
+            print(result)
+            continue
+        if num == "-" :
+            secondNumber = int (input("Podaj drugą liczbę : "))
+            result = firstNumber - secondNumber
+            firstNumber = result
+            print(result)
+            continue
+        if num == "*" :
+            secondNumber = int (input("Podaj drugą liczbę : "))
+            result = firstNumber * secondNumber
+            firstNumber = result
+            print(result)
+            continue
+        if num == "/" :
+            secondNumber = int (input("Podaj drugą liczbę : "))
+            result = firstNumber / secondNumber
+            firstNumber = result
+            print(result)
+            continue
+        if num == "**" :
+            secondNumber = int (input("Podaj drugą liczbę : "))
+            result = firstNumber ** secondNumber
+            firstNumber = result
+            print(result)
+            continue
+
+
